f8a5499e393289ea3b2b357a4a29afc109dbacc9
aritreyJona <51917383+aritreyJona@users.noreply.github.com>
2020-03-27 09:41:03 -0300

    funkt: mehrere Files; ohne <Modal>

---
+++ App.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  FlatList
+} from "react-native";
+
+import GoalItem from "./components/GoalItem";
+import GoalInput from "./components/GoalInput";
+
+export default function App() {
+  const [CourseGoals, setCourseGoals] = useState([]);
+
+  const addGoalHandler = goalTitle => {
+    setCourseGoals(currentGoals => [
+      ...currentGoals,
+      { id: Math.random().toString(), value: goalTitle }
+    ]);
+  };
+
+  //wenn wir  id={itemData.item.id} bei GoalItem wegnehmen ist goalId undefined
+  //woher weiß die function, dass es mit Id vergleichen muss?
+  //vergl. automatisch gleiche pros (id mit id von goalitem)
+  const removeGoalHandler = goalId => {
+    setCourseGoals(currentGoals => {
+      return currentGoals.filter(goal => goal.id !== goalId);
+    });
+  };
+
+  return (
+    <View style={styles.screen}>
+      <GoalInput onAddGoal={addGoalHandler} />
+
+      <FlatList
+        keyExtractor={(item, index) => item.id}
+        // wenn item key als attribut hat wird das genommen und kein key extractor gebraucht
+        data={CourseGoals}
+        renderItem={itemData => (
+          <GoalItem
+            // ALTERNATIV: id fuer onDelete: onDelete={removeGoalHandler.bind(this,itemData.item.id)}
+            id={itemData.item.id}
+            onDelete={removeGoalHandler}
+            title={itemData.item.value}
+          />
+        )}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50,
+    flexDirection: "column"
+  }
+});
